fd992a50199adbdbc0c12b42930fdac2c8ecead1
Adam Tvedt-Østreng <ad4mski04@gmail.com>
2025-10-02 23:29:25 +0200

    Initial commit

---
+++ aksjeportefolje.py
@@ -0,0 +1,42 @@
+class Aksje():
+    def __init__(self, navn, antall, kjopspris, naapris):
+        self.navn = navn
+        self.antall = antall
+        self.kjopspris = kjopspris
+        self.naapris = naapris
+
+    def __str__(self):
+        return f"{self.navn}: {self.antall} aksjer, {self.naapris}kr (kjøpt for {self.kjopspris}kr)"
+
+    def verdinaa(self):
+        return self.antall * self.naapris
+
+    def verdida(self):
+        return self.antall * self.kjopspris
+
+class Portefolje():
+    def __init__(self):
+        self.aksjer = []
+    
+    def legg_til_aksje(self, navn:str, antall:int, kjopspris:int, naapris:str):
+        aksje = Aksje(navn, antall, kjopspris, naapris)
+        self.aksjer.append(aksje)
+        print(f"La til {aksje} i porteføljen.")
+    
+    def kontoverdi(self):
+        verdi_aksjer = []
+        for aksje in self.aksjer:
+            verdi_aksjer.append(aksje.verdinaa())
+        return sum(verdi_aksjer)
+
+    def vis_oversikt(self):
+        for aksje in self.aksjer:
+            print(aksje)
+        print(f"Totalverdi i porteføljen: {self.kontoverdi()}kr")
+    
+
+p = Portefolje()
+p.legg_til_aksje("Apple", 10, 150, 175)
+p.legg_til_aksje("Tesla", 5, 200, 180)
+p.vis_oversikt()
+
