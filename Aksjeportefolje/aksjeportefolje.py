398999e1bf8440019420df79340b6cbdde35e357
Anders Erikstad <anderserikstad@eduroam-193-157-237-26.wlan.uio.no>
2025-10-03 12:03:42 +0200

    better readme + include Aksjeportefolje files

---
+++ Aksjeportefolje/aksjeportefolje.py
@@ -0,0 +1,39 @@
+class Aksje():
+    def __init__(self, navn:str, antall:int, kjopspris:float, naapris:float):
+        self.navn = navn
+        self.antall = antall
+        self.kjopspris = kjopspris
+        self.naapris = naapris
+
+    def __str__(self) -> str:
+        return f"{self.navn}: {self.antall} aksjer, {self.naapris}kr (kjøpt for {self.kjopspris}kr)"
+
+    def verdinaa(self) -> float:
+        return self.antall * self.naapris
+
+    def verdida(self) -> float:
+        return self.antall * self.kjopspris
+
+class Portefolje():
+    def __init__(self):
+        self.aksjer = []
+    
+    def legg_til_aksje(self, navn:str, antall:int, kjopspris:float, naapris:float):
+        aksje = Aksje(navn, antall, kjopspris, naapris)
+        self.aksjer.append(aksje)
+        print(f"La til {aksje} i porteføljen.")
+    
+    def kontoverdi(self):
+        return sum(aksje.verdinaa() for aksje in self.askjer)
+
+    def vis_oversikt(self):
+        for aksje in self.aksjer:
+            print(aksje)
+        print(f"Totalverdi i porteføljen: {self.kontoverdi()}kr")
+    
+
+p = Portefolje()
+p.legg_til_aksje("Apple", 10, 150, 175)
+p.legg_til_aksje("Tesla", 5, 200, 180)
+p.vis_oversikt()
+
