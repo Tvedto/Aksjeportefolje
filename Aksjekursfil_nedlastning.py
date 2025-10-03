10dcaebedb7bed6062add67b027016557ee1af68
Adam Tvedt-Østreng <ad4mski04@gmail.com>
2025-10-03 11:55:25 +0200

    La til aksjekursfiler

--- Aksjekursfil_nedlastning.py
+++ Aksjekursfil_nedlastning.py
@@ -11,7 +11,7 @@ data = ["AAPL", "TSLA", "MSFT", "NVDA", "SNAP"] #noen eksempler på aksjer
 
 for d in data:
 
-    download = yf.download(d, start="2023-01-01", end="2024-01-01")
+    download = yf.download(d, start="2021-01-01", end="2024-01-01")
 
     with open(f"{d}.txt", "w") as f:
         for pris in download["Close"].values:  #Close er values gjør at det bare blir tallene som printes
